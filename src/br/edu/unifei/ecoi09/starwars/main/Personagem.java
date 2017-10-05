6a7fb67fa0320ce1a91476289c1986745602b889
Alencar <alencarantonio@unifei.edu.br>
2017-10-05 14:31:58 -0300

    - Update 1.0.3
    Criacão da classe personagem
    
    Alencar

---
+++ src/br/edu/unifei/ecoi09/starwars/main/Personagem.java
@@ -0,0 +1,11 @@
+package br.edu.unifei.ecoi09.starwars.main;
+
+public class Personagem {
+    String Name;
+    int power;
+    int agility;
+    int precision;
+
+
+// Definiçao dos atributos dos personagens
+}
